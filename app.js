a78d33de2b3ca0c5c9fd723bb2df92db499f8113
Shubham <shubham2002bharat@gmail.com>
2021-06-18 04:53:26 +0530

    minion-translate-app

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
-var btnTranslate = document.querySelector("#btn-translate")
-var txtInput = document.querySelector("#input-txt")
-var outputDiv = document.querySelector("#output-div")
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#input-txt");
+var outputDiv = document.querySelector("#output-div");
+// var serverURL ="https://api.funtranslations.com/translate/yoda.json";
 var serverURL ="https://api.funtranslations.com/translate/minion.json";
 function getTranslatedURL(text){
     return serverURL + "?" + "text=" + text
 }
-function errorHandler(){
+function errorHandler(error){
     console.log("error",error)
     alert("Ooops! something went wrong")
 }
 function clickHandler(){
+var inputText= txtInput.value;    
 fetch(getTranslatedURL(inputText))
-.then(response => response.json)
+.then(response => response.json())
 .then(json =>{
-    var translatedText=json.contents.translated
-    outputDiv.value = translatedText
+    var translatedText=json.contents.translated;
+    outputDiv.innerText = translatedText;
 })
 .catch(errorHandler)
-}
+};
 btnTranslate.addEventListener("click",clickHandler);
